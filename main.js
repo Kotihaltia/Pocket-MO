66f2cd3f1dee7d7f6d4bdd03f5d474130b17e911
Kotihaltia <ilernfel@gmail.com>
2020-11-19 20:27:11 +0300

    homework-2
    Моя не смог не дублирывать код. Но моя думать над это)

--- main.js
+++ main.js
@@ -1,21 +1,68 @@
+const $btn = document.getElementById('btn-kick');
+const $press = document.getElementById('press-smash');
 
-const firstRow = 'мама мыла раму';
-const secondRow = 'собака друг человека';
+const character = {
+    name: 'Pikachu',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-character'),
+    elProgressbar: document.getElementById('progressbar-character'),
+}
 
-function getRow(firstRow, secondRow) {
+const enemy = {
+    name: 'Charmander',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-enemy'),
+    elProgressbar: document.getElementById('progressbar-enemy'),
+}
 
+$btn.addEventListener('click', function () {
+    console.log('Kick');
+    changeHP(random(20), character);
+    changeHP(random(20), enemy);
+});
+
+$press.addEventListener('click', function () {
+    console.log('press-smash');
+    changeHP(random(10), character);
+    changeHP(random(10), enemy);
+});
+
+function init() {
+    console.log('Start Game!');
+    renderHP(character);
+    renderProgressbarHP(enemy);
+}
+
+<<<<<<< Updated upstream
     let firstRowL = 0;
     let secondRowL = 0;
+=======
+function renderHP(person) {
+    renderHPLife(person);
+    renderProgressbarHP(person);
+}
+>>>>>>> Stashed changes
 
-    for (let i = 0; i < firstRow.length; i++) {
+function renderHPLife(person) {
+    person.elHP.innerText = person.damageHP + ' / ' + person.defaultHP;
+}
 
+<<<<<<< Updated upstream
         if (firstRow.charAt(i) === 'а') {
             firstRowL++;
         }
     }
+=======
+function renderProgressbarHP(person) {
+    person.elProgressbar.style.width = person.damageHP + '%';
+}
+>>>>>>> Stashed changes
 
-    for (let i = 0; i < secondRow.length; i++) {
+function changeHP(count, person) {
 
+<<<<<<< Updated upstream
         if (secondRow.charAt(i) === 'а') {
             secondRowL++;
         }
@@ -26,14 +73,24 @@ function getRow(firstRow, secondRow) {
     }
     else {
         return secondRow;
+=======
+    if (person.damageHP < count) {
+        person.damageHP === 0;
+        alert('В зале есть доктор? ' + person.name + ' плохо!');
+        $btn.disabled = true;
+        $press.disabled = true;
+
+    } else {
+        person.damageHP -= count;
+>>>>>>> Stashed changes
     }
 
+    renderHP(person);
 }
 
-console.log(getRow(firstRow, secondRow));
+function random(num) {
+    return Math.ceil(Math.random() * num);
+}
 
-
-
-
-
+init();
 
